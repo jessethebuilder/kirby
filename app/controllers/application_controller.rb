4d426077843efcd52c3bf22b2d09bd659b91ae1e
Jesse Farmer <jessethebuilder@yahoo.com>
2017-03-17 18:53:14 -0700

    Inline login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,19 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    url = request.referer
+    if url == new_user_session_url || url == new_user_registration_url
+      super
+    else
+      stored_location_for(resource) || url || root_path
+    end
+  end
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+    # devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
   end
 end
